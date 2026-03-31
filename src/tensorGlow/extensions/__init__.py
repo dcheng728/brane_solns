c23b0efe6ecfb526c9f07a56d25bd7efb4300e9f
Chang Cheng <dcheng728@gmail.com>
2026-03-31 17:13:44 +0100

    init commit of the branch

--- src/tensorGlow/extensions/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Extensions: dimensional reduction, block metrics."""
-
-from .dimensional_reduction import IndexSplit
-from .block_metric import BlockMetric, ScalarField
-
-__all__ = [
-    'IndexSplit',
-    'BlockMetric', 'ScalarField',
-]
